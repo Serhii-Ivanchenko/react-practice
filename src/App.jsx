f755a2250783656b3e1904d29a0af6842117679a
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-05-12 22:53:57 +0300

    task 2 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import {
   ForbesList,
 } from 'components';
 
-import article from "./data/article.json"
+import article from './data/article.json';
+import stats from 'data/stats.json';
 
 export const App = () => {
   return (
@@ -26,11 +27,10 @@ export const App = () => {
         />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />;
 
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
-
         <Heading title="Task 4 Crypto history" top bottom />
         <CryptoHistory />
       </Container>
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,14 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './Statistics.module.css';
+import { IconContext } from 'react-icons';
+
+export const StatisticsItem = ({ total, title, icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ color: 'black', size: '35px' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
